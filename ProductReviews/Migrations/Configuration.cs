75814e9e20afeee3d0dfe145880fb19632703d55
ferlerrorr <fjohn.d.javier@gmail.com>
2021-03-12 07:35:22 +0800

    AspRESTApi_01

---
+++ ProductReviews/Migrations/Configuration.cs
@@ -0,0 +1,30 @@
+namespace ProductReviews.Migrations
+{
+    using ProductReviews.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ProductReviews.Models.ProductReviewsContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ProductReviews.Models.ProductReviewsContext context)
+        {
+            context.Products.AddOrUpdate(p => p.ProductId,
+                 new Product { Name = "Product 1", Category = "Category 1", Price = 200 },
+                 new Product { Name = "Product 2", Category = "Category 2", Price = 500 },
+                 new Product { Name = "Product 3", Category = "Category 3", Price = 700 }
+                 );
+
+            context.Reviews.AddOrUpdate(r => r.ReviewId,
+                            new Review { Title = "Review 1", Description = "Test review 1", ProductId = 1 },
+                            new Review { Title = "Review 2", Description = "Test review 2", ProductId = 1 }
+                            );
+        }
+    }
+}
